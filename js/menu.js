e18f8ac871e691311808b22bc7052f01c80cf2d5
Esteban Prieto <esripri@gmail.com>
2021-12-06 11:10:05 -0500

    EP blog beta 0.1 6/12/21 11:09AM

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+document.getElementById("icon-menu").addEventListener("click", show_menu);
+
+function show_menu() {
+
+    console.log("Hamburgesa")
+    // document.getElementById("move-content").classList.toggle('move-container-all');
+    document.getElementById("show-menu").classList.toggle('show-side');
+}
